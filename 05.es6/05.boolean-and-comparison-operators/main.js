9dc3973a2c54721ed893eabccd846c08423f770a
ahadsts9901 <ahadsts990@gmail.com>
2025-11-26 15:54:18 +0500

    if else

---
+++ 05.es6/05.boolean-and-comparison-operators/main.js
@@ -0,0 +1,42 @@
+// true, false
+// 0, 1
+
+// console.log("8" === "6")
+
+// double equal ==> only compare values
+// triple equal ==> compare values and data_types both
+
+
+// console.log(true + 1 - true)
+
+
+
+
+// comparison operators
+
+
+
+// NOT operator
+// console.log(!1967)
+// console.log(!-23)
+// console.log(!"")
+
+// any number will be true   ===> 1, 345, -23, 0.001     ===> true
+// only 0 will be false      ===> 0                      ===> false
+
+// any string which has some characters will be true ===> "some name" ===> true
+// any string which is empty will be false           ===> ""          ===> false
+
+// console.log(6 !== "6")
+
+// ===  ===> triple equals              ===> check value and data_type
+// ==   ===> double equals              ===> check only value
+// !    ===> not operator               ===> opposite any value
+// >    ===> greater than               ===> check value greater or not
+// <    ===> less than                  ===> check value less or not
+// !=   ===> not equal                  ===> left aur right values equal nahi hona chhaiye
+// !==  ===> not double equal           ===> left aur right values equal nahi hona chhaiye aur datatype bhi equal nahi hona chhaiye
+// >=   ===> greater than and equal to  ===> ya to bara ho, ya barabar ho
+// <=   ===> less than and equal to     ===> ya to chhota ho, ya barabar ho
+
+// console.log(48 <= 48)
