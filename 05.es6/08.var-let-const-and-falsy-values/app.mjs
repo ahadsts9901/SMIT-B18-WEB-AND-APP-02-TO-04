291a82202a36c0e26ebea50af09588b6c393bf25
ahadsts9901 <ahadsts990@gmail.com>
2025-12-05 19:58:55 +0500

    code

---
+++ 05.es6/08.var-let-const-and-falsy-values/app.mjs
@@ -0,0 +1,32 @@
+let name_1 = "Junaid"
+var newName = "Ali"
+const another_name = "Sarim"
+
+// another_name = "hello"
+
+// let: dont allow to redeclare the variable with same name
+// var: allow to redeclare the variable with same name, but override the value
+// const: doesnt allow to redeclare & override the variable
+
+// number
+// string
+// boolean ==> true, false
+
+// true: 
+//      any number except zero
+//      no empty string
+
+
+
+// false: 
+//      zero
+//      empty string
+
+let val = null
+let val2 = undefined
+
+console.log(myNewAccountNumber)
+
+console.log(val)
+console.log(val2)
+
---
+++ 05.es6/09.array/app.mjs
@@ -0,0 +1,40 @@
+// let cities = [
+//     "Karachi",
+//     "Baltimore",
+//     "Chicago",
+//     "Denver",
+//     "Los Angeles",
+//     "Seattle",
+//     "Islamabad"
+// ]
+
+// cities[2] = "Lahore"
+
+// console.log(cities)
+
+
+// 2D array
+
+// let $2d_array = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ]
+
+// console.log($2d_array)
+
+
+// array methods
+
+let names = ["Junaid", "Waseem", "Irfan", "Saeed"]
+
+// names.push("Ali")     =====> ADD to the end of the array
+// names.pop()           =====> REMOVE from the end of the array
+// names.unshift("Ali")  =====> ADD from the start of the array
+// names.shift()         =====> REMOVE from the start of the array
+
+console.log(names)
+
+
+
+
