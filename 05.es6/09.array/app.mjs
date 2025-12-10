961b49b977cf548577c985ad2a027b09e0f07d16
ahadsts9901 <ahadsts990@gmail.com>
2025-12-10 18:41:15 +0500

    class code

--- 05.es6/09.array/app.mjs
+++ 05.es6/09.array/app.mjs
@@ -33,8 +33,42 @@ let names = ["Junaid", "Waseem", "Irfan", "Saeed"]
 // names.unshift("Ali")  =====> ADD from the start of the array
 // names.shift()         =====> REMOVE from the start of the array
 
-console.log(names)
+// console.log(names)
 
+var my_name = "John Doe"
+var my_name = "Ali Raza"
+my_name = "Hello"
+
+let my_city = "Karachi"
+// let my_city = "Islamabad"
+my_city = "Lahore"
+
+const my_country = "Pakistan"
+// my_country = "Karachi"
+
+let my_arr = ["Islamabad", "Lahore", "Hyderabad"]
+// console.log(my_arr)
+// my_arr.unshift("Karachi", "Kashmir")
+// console.log(my_arr)
+
+// let new_arr = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+// ]
+
+// console.log(new_arr[1][1])
+
+//      ===========>0   1   2   3   4   5   6   7   8
+let my__numbers = [12, 23, 45, 56, 74, 88, 90, 23, 56]
+
+// let section = my__numbers.slice(2, 6)
+
+// console.log("section===> ", section)
+// console.log("numbers===> ", my__numbers)
+
+my__numbers.splice(5, 0, "madni", "bilawal", "web", "saylani")
+console.log(my__numbers)
 
 
 
