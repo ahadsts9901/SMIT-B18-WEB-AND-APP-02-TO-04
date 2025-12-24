d526507473bc0801096047678faf506ed04b2a7d
ahadsts9901 <ahadsts990@gmail.com>
2025-12-24 15:54:08 +0500

    class code

---
+++ 05.es6/15.for-loop-booleans/main.mjs
@@ -0,0 +1,23 @@
+let cleanest_cities = [
+    "kashmir", "lahore", "islamabad", "istanbul",
+    "new york", "canada", "california",
+    "chicago", "dubai", "tokyo",
+]
+
+let userCity = prompt("enter your city name")
+let cityFound = false
+
+// islamabad
+
+for (let i = 0; i < cleanest_cities.length; i++) {
+    if (cleanest_cities[i] === userCity) {
+        cityFound = true
+        break
+    }
+}
+
+if (cityFound) {
+    alert("your city is cleanest")
+} else {
+    alert("your city not found in the list")
+}
