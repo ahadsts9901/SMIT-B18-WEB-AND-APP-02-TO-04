0587b32914becd9b34440ab4a8469f4e3cd476fe
ahadsts9901 <ahadsts990@gmail.com>
2026-01-02 15:54:52 +0500

    coed

---
+++ 05.es6/20.object/main.mjs
@@ -0,0 +1,22 @@
+// console.log("string")
+// console.log(87238)
+// console.log(true)
+
+// const myArr = ["dhkahd", "dasd", "dhaksh"]
+// console.log(myArr)
+
+// let username = "ali"
+
+let my_user = {
+    name: "Junaid",
+    age: 34,
+    jobHolder: true,
+    languages: ["html", "css", "js"],
+    fatherDetails: {
+        name: "Suleman",
+        age: 60,
+        alive: true,
+    }
+}
+
+console.log(my_user.fatherDetails)
