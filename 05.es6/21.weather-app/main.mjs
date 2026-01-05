6414eb8c339c83f93338f66c35115742fad2e701
ahadsts9901 <ahadsts990@gmail.com>
2026-01-05 15:54:51 +0500

    class code

---
+++ 05.es6/21.weather-app/main.mjs
@@ -0,0 +1,15 @@
+async function get_weather(event) {
+    event.preventDefault()
+    const cityName = document.querySelector("#cityName").value
+    const api_key = "26ae9a0b27a97215beaa4b5df9aa15fb"
+
+    const response = await axios.get(`http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${api_key}&unit=metrics`)
+
+    const kelvin = response.data.list[0].main.temp;
+    const celsius = kelvin - 273.15;
+
+    document.querySelector(".country").innerText = response.data.city.country
+    document.querySelector(".temp").innerText = celsius.toFixed(1) + " °C"
+
+    console.log(response.data.list[0].main)
+}
