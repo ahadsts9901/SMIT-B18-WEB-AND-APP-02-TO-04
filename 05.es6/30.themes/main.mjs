e948cb2800e6efc0ca471a984941f9febab5fd6b
ahadsts9901 <ahadsts990@gmail.com>
2026-01-26 15:56:50 +0500

    class code

---
+++ 05.es6/30.themes/main.mjs
@@ -0,0 +1,11 @@
+function handle_theme_change(theme_class, nextButtonClass) {
+    document.querySelector('body').className = theme_class
+
+    // const all_buttons = document.querySelectorAll('button')
+    // for (let i = 0; i < all_buttons.length; i++) {
+    //     all_buttons[i].style.display = 'none'
+    // }
+
+    // document.querySelector(`.${nextButtonClass}`).style.display = 'block'
+
+}
