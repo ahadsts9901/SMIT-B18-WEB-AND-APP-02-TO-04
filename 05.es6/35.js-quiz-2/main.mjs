ae6402a6e15f575b040844278fcf5a7d1075d31e
ahadsts9901 <ahadsts990@gmail.com>
2026-02-09 16:01:29 +0500

    class code

---
+++ 05.es6/35.js-quiz-2/main.mjs
@@ -0,0 +1,83 @@
+// // charAt
+// let text = "Hello"
+// console.log(text.charAt(20))
+
+
+// // string to number
+// let my_num = "1234"
+// console.log(Number(my_num))
+// console.log(parseInt(my_num))
+
+
+// // global variable access and change
+// let global_variable = "hello world"
+
+// function my_fun() {
+//     console.log("my function running")
+//     global_variable = "changed"
+// }
+
+// my_fun()
+// console.log(global_variable)
+
+
+// // global abd local variable same name
+// let global_var = "hello world"
+
+// function my__function() {
+//     let global_var = "function local variable"
+// }
+
+// my__function()
+
+
+
+// // include
+// let username = "shahrukh"
+// console.log(username.includes("shah"))
+
+
+
+// // parameters vs argument
+
+// // parameter
+// function my___func(firstname, lastname) {
+//     console.log(firstname)
+//     console.log(lastname)
+// }
+
+// // argument
+// my___func("Hamza", "Ali")
+
+
+// void and method
+
+// // void
+// function calculate_percentage(total, obt) {
+//     // those functions who does not return any value
+//     return obt / total * 100
+// }
+
+// function calculate_grade() {
+//     let percentage = calculate_percentage(140, 80)
+//     console.log(percentage)
+// }
+// calculate_grade()
+
+
+
+// // method
+// function calculate_percentage(total, obt) {
+//     // those functions who does not return any value
+//     return obt / total * 100
+// }
+
+// console.log(calculate_percentage(200, 130))
+
+// let my_name = "heLlO WoRld"
+// console.log(my_name.toUpperCase())
+
+
+// // date questions
+// let date = new Date("02-01-2024")
+// console.log(date)
