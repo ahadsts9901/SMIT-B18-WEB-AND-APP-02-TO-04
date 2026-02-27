5efd1b299c43435bca4659dd053b87f9af373359
ahadsts9901 <ahadsts990@gmail.com>
2026-02-27 12:54:15 +0500

    code

---
+++ 05.es6/46.iterators/main.mjs
@@ -0,0 +1,38 @@
+// for of
+
+let numbers = [1, 2, 3, 4, 5]
+
+// array
+// for (const abcd of numbers) {
+//     console.log(abcd)
+// }
+
+// numbers.map((num) => {
+//     console.log(num)
+// })
+
+
+
+
+
+// for in
+
+let user = {
+    first_name: "jumbo",
+    last_name: "dumbo",
+    age: 40,
+    lanugages: ["html", "css", "js"],
+    address: "karachi",
+    vehicle: "honda 70",
+    // key: "not available"
+}
+
+// console.log(user)
+// console.log(Object.keys(user))
+// console.log(Object.values(user))
+
+// for (const hello in user) {
+//     console.log(hello)
+//     console.log(user[hello])
+//     // console.log(user["first_name"])
+// }
