1696f63414d84f9510cf8c04de383a6a57aa8995
ahadsts9901 <ahadsts990@gmail.com>
2026-04-27 15:53:01 +0500

    code

---
+++ 05.es6/68.import-export/functions.mjs
@@ -0,0 +1,23 @@
+export const calculatePercentage = (obtMarks, totalMarks) => {
+    console.log("functions.mjs")
+    console.log(obtMarks / totalMarks * 100)
+}
+
+export const greetUser = (username) => {
+    console.log("hello " + username)
+}
+
+export const username = "Anas"
+export const languages = ["html", "css", "js", "ts"]
+export const book = {
+    bookname: "abc book",
+    author: "abc author",
+    price: 230,
+}
+
+// export {
+//     calculatePercentage, greetUser,
+//     username,
+//     languages,
+//     book,
+// }
---
+++ 05.es6/68.import-export/index.mjs
@@ -0,0 +1,28 @@
+import {
+    book,
+    calculatePercentage,
+    greetUser,
+    username as myUsername,
+    languages,
+} from "./functions.mjs"
+
+import {
+    calculatePercentage as myFunction,
+} from "./other-functions.mjs"
+
+import addFun from "./other-functions.mjs"
+
+console.log("hello nodejs")
+console.log(1 + 5)
+
+calculatePercentage(130, 200)
+greetUser("Jamal")
+
+myFunction(70, 170)
+
+console.log(myUsername)
+console.log(languages)
+console.log(book)
+
+addFun(13, 12)
+
