7f71db459380415b86e28dd2ba4b73561e86261c
ahadsts9901 <ahadsts990@gmail.com>
2026-05-04 16:02:31 +0500

    code

--- 06.firebase/02.crud/app.mjs
+++ 06.firebase/02.crud/app.mjs
@@ -1,15 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs, orderBy, doc, deleteDoc, setDoc } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDU0CQ8vxduQlr1qZOH1dWjp3XfYE0IJuc",
-    authDomain: "smit-b18.firebaseapp.com",
-    projectId: "smit-b18",
-    storageBucket: "smit-b18.firebasestorage.app",
-    messagingSenderId: "243844172855",
-    appId: "1:243844172855:web:49192254a79ffc193ec3b2",
-    measurementId: "G-JCL8ZL1GN2"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDU0CQ8vxduQlr1qZOH1dWjp3XfYE0IJuc",
+//     authDomain: "smit-b18.firebaseapp.com",
+//     projectId: "smit-b18",
+//     storageBucket: "smit-b18.firebasestorage.app",
+//     messagingSenderId: "243844172855",
+//     appId: "1:243844172855:web:49192254a79ffc193ec3b2",
+//     measurementId: "G-JCL8ZL1GN2"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
