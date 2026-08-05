b1b37307199a794d6796520db5548de04750d945
ahadsts9901 <ahadsts990@gmail.com>
2026-08-05 15:51:44 +0500

    code

---
+++ 08.server/07.mongodb/server.mjs
@@ -0,0 +1,29 @@
+import "dotenv/config"
+import express from "express"
+import { postRoutes } from "./routes/index.mjs"
+import cors from "cors"
+import { connect_database } from "./libs/mongodb.mjs"
+
+const app = express()
+
+// body parser
+app.use(express.json())
+
+// cors
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: "*"
+}))
+
+const PORT = process.env.PORT || 3001
+
+app.get("/", (req, res) => {
+    res.send("hello world server")
+})
+
+app.use("/api/v1", postRoutes)
+
+app.listen(PORT, () => {
+    console.log("server is running...")
+    connect_database()
+})
